c0a4199feca80517e365be39253267fff9328d5b
Ishan Khare <ishankhare07@gmail.com>
2017-04-25 16:06:06 +0530

    accept a subset of monkey in lexer
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,8 +30,42 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
     return token.Token{tokenType, string(ch)}
 }
 
+func (l *Lexer) readIdentifier() string {
+    position := l.position
+    for isLetter(l.ch) {
+        l.readChar()
+    }
+
+    return l.input[position:l.position]     // return a slice of string (identifier)
+}
+
+func (l *Lexer) readNumber() string {
+    position := l.position
+    for isDigit(l.ch) {
+        l.readChar()
+    }
+
+    return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+    return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+    for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+        l.readChar()
+    }
+}
+
 func (l *Lexer) NextToken() token.Token {
     var tok token.Token
+
+    l.skipWhitespace()
 
     switch l.ch {
         case '=':
@@ -53,6 +87,18 @@ func (l *Lexer) NextToken() token.Token {
         case 0:
             tok.Literal = ""
             tok.Type = token.EOF
+        default:
+            if isLetter(l.ch) {
+                tok.Literal = l.readIdentifier()
+                tok.Type = token.LookupIdent(tok.Literal)
+                return tok  // return and don't call next readChar, as positions already forwarded in readIdentifier
+            } else if isDigit(l.ch) {
+                tok.Type = token.INT
+                tok.Literal = l.readNumber()
+                return tok
+            } else {
+                return newToken(token.ILLEGAL, l.ch)
+            }
     }
 
     l.readChar()
