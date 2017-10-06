9be11b814ca59d9443c55358bb65f8b932515b47
Ishan Khare <ishankhare07@gmail.com>
2017-10-06 19:30:42 +0530

    add ast and parser
    parser now has ability to parse let statements
    
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

---
+++ ast/ast.go
@@ -0,0 +1,66 @@
+package ast
+
+import "gitlab.com/ishankhare07/monkey-lang/token"
+
+type Node interface {
+    TokenLiteral() string
+}
+
+type Statement interface {
+    Node
+    statementNode()
+}
+
+type Expression interface {
+    Node
+    expressionNode()
+}
+
+type Program struct {
+    Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+    if len(p.Statements) > 0 {
+        return p.Statements[0].TokenLiteral()
+    } else {
+        return ""
+    }
+}
+
+// define type LetStatement
+
+type LetStatement struct {
+    Token token.Token
+    Name *Identifier
+    Value Expression
+}
+
+// implement Node interface
+
+func (ls *LetStatement) TokenLiteral() string {
+    return ls.Token.Literal
+}
+
+// implement Statement interface
+
+func (ls *LetStatement) statementNode() {}
+
+// define Identifier type
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+// implement Node interface
+
+func (i *Identifier) TokenLiteral() string {
+    return i.Token.Literal
+}
+
+// implement Expression interface
+
+func (i *Identifier) expressionNode() {}
+
+
