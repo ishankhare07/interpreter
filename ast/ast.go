74ca258983981a84eef995eb5302bfd7586958fa
Ishan Khare <ishankhare07@gmail.com>
2017-10-08 19:39:30 +0530

    parse return statements
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- ast/ast.go
+++ ast/ast.go
@@ -3,43 +3,43 @@ package ast
 import "gitlab.com/ishankhare07/monkey-lang/token"
 
 type Node interface {
-    TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-    Node
-    statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-    Node
-    expressionNode()
+	Node
+	expressionNode()
 }
 
 type Program struct {
-    Statements []Statement
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
-    if len(p.Statements) > 0 {
-        return p.Statements[0].TokenLiteral()
-    } else {
-        return ""
-    }
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
 }
 
 // define type LetStatement
 
 type LetStatement struct {
-    Token token.Token
-    Name *Identifier
-    Value Expression
+	Token token.Token
+	Name  *Identifier
+	Value Expression
 }
 
 // implement Node interface
 
 func (ls *LetStatement) TokenLiteral() string {
-    return ls.Token.Literal
+	return ls.Token.Literal
 }
 
 // implement Statement interface
@@ -49,18 +49,32 @@ func (ls *LetStatement) statementNode() {}
 // define Identifier type
 
 type Identifier struct {
-    Token token.Token
-    Value string
+	Token token.Token
+	Value string
 }
 
 // implement Node interface
 
 func (i *Identifier) TokenLiteral() string {
-    return i.Token.Literal
+	return i.Token.Literal
 }
 
 // implement Expression interface
 
 func (i *Identifier) expressionNode() {}
 
+// define type ReturnStatement
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+// implement Node interface
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
+// implement Statement interface
+
+func (rs *ReturnStatement) statementNode() {}
