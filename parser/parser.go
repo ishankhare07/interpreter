a754668b45f57be4e367c4b87f5ec14ef8ccf393
Ishan Khare <ishankhare07@gmail.com>
2017-10-08 15:41:19 +0530

    finish parsing let statements
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
     "gitlab.com/ishankhare07/monkey-lang/ast"
     "gitlab.com/ishankhare07/monkey-lang/lexer"
     "gitlab.com/ishankhare07/monkey-lang/token"
+    "fmt"
 )
 
 type Parser struct {
@@ -11,10 +12,14 @@ type Parser struct {
 
     curToken    token.Token
     peekToken   token.Token
+    errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+    p := &Parser{
+        l: l,
+        errors: []string{},
+    }
 
     p.nextToken()
     p.nextToken()
@@ -27,6 +32,15 @@ func (p *Parser) nextToken() {
     p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
     program := &ast.Program{}
     program.Statements = []ast.Statement{}
@@ -34,7 +48,7 @@ func (p *Parser) ParseProgram() *ast.Program {
     for p.curToken.Type != token.EOF {
         stmt := p.parseStatement()
         if stmt != nil {
-           program.Statements = append(program.Statements, stmt) 
+           program.Statements = append(program.Statements, stmt)
         }
         p.nextToken()
     }
@@ -84,6 +98,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
 }
